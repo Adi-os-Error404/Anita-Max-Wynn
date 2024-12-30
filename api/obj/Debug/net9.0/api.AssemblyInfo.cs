9dc1503db287a5d06123d608338c6eef559b72eb
Aditya Patel <adityap902021@gmail.com>
2024-12-30 13:21:48 -0800

    add models

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f7c06547937246000ddc0999675a0091261c546")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b011c9b5ba1409acc33bb2444a7d799b4a20480")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

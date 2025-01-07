e6e91d9150c5c2eb6e486600aba3b5b7fcdac24b
Aditya Patel <adityap902021@gmail.com>
2025-01-06 19:38:40 -0800

    added comment delete api

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -55,6 +55,17 @@ namespace api.Controllers
             return CreatedAtAction(nameof(GetById), new{ id = commentModel }, commentModel.ToCommentDto());
         }
 
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var commentModel = await _commentRepo.DeleteAsync(id);
+            if (commentModel == null) {
+                return NotFound("Comment does not exist");
+            }
+            return Ok(commentModel);
+        }
+
     
     }
 }
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e277dc36c14dbd4214b794e46c1e7f7999cb112")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b913dd33902335dcdb6a79944ae166b53b08660")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
